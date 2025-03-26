abe90f2634b53e8fa3e0ceb847072c2c8c850fa7
SwagMuffin88 <katrilotamois@outlook.com>
2025-03-26 20:55:32 +0200

    add API endpoints for sorting flights by price and departure date (asc, desc) and filtering by destination (airport name)

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
 import FlightsTable from "../flights/FlightsTable";
+import Hero from "../hero/Hero";
 
 const Home = ({flights}) => {
     return (
       <div>
+        <Hero/>
         <FlightsTable flights={flights}/>
       </div>
     )
