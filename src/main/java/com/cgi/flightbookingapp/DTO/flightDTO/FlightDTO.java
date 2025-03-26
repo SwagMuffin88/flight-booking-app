f1d43d090ad7b5022e4d9481d39415fcfe00900d
SwagMuffin88 <katrilotamois@outlook.com>
2025-03-26 17:16:08 +0200

    update OpenAPI spec file to be up to date with the current API structure

--- src/main/java/com/cgi/flightbookingapp/DTO/flightDTO/FlightDTO.java
+++ src/main/java/com/cgi/flightbookingapp/DTO/flightDTO/FlightDTO.java
@@ -2,9 +2,12 @@ package com.cgi.flightbookingapp.DTO.flightDTO;
 
 import com.cgi.flightbookingapp.DTO.locationDTO.LocationDTO;
 import com.cgi.flightbookingapp.DTO.planeDTO.PlaneDTO;
+import com.cgi.flightbookingapp.DTO.seatDTO.FlightSeatDTO;
+import com.cgi.flightbookingapp.model.seat.FlightSeat;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public record FlightDTO(
         Long id,
@@ -12,6 +15,7 @@ public record FlightDTO(
         LocationDTO destination,
         String planeName,     // Instead of a Plane DTO, which causes cyclical dependency
         LocalDateTime departureTime,
+        List<FlightSeatDTO> seats,
         BigDecimal price
 ) {
 }
--- src/main/java/com/cgi/flightbookingapp/DTO/flightDTO/FlightDTOMapper.java
+++ src/main/java/com/cgi/flightbookingapp/DTO/flightDTO/FlightDTOMapper.java
@@ -1,19 +1,29 @@
 package com.cgi.flightbookingapp.DTO.flightDTO;
 
 import com.cgi.flightbookingapp.DTO.locationDTO.LocationDTOMapper;
+import com.cgi.flightbookingapp.DTO.seatDTO.FlightSeatDTO;
+import com.cgi.flightbookingapp.DTO.seatDTO.FlightSeatDTOMapper;
 import com.cgi.flightbookingapp.model.Flight;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Service @RequiredArgsConstructor
 public class FlightDTOMapper implements Function<Flight, FlightDTO> {
     private final LocationDTOMapper locationDTOMapper;
-//    private final PlaneDTOMapper planeDTOMapper;
+    private final FlightSeatDTOMapper flightSeatDTOMapper;
     
     @Override
     public FlightDTO apply(Flight flight) {
+
+        List<FlightSeatDTO> seats = flight.getFlightSeats()
+                .stream()
+                .map(flightSeatDTOMapper)
+                .collect(Collectors.toList());
+        
         return new FlightDTO(
                 flight.getId(),
                 locationDTOMapper.apply(
@@ -22,6 +32,7 @@ public class FlightDTOMapper implements Function<Flight, FlightDTO> {
                         flight.getDestination()),
                 flight.getPlane().getName(),
                 flight.getDepartureTime(),
+                seats,
                 flight.getPrice()
         );
     }
